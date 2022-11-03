91458d67e1f05fa9ef9a132244a4cecf64dd8fba
Luis Indio <luisindio2000@gmail.com>
2022-11-03 05:54:52 -0400

    microservicio CheckIn

---
+++ backend/CheckIn/Domain.Checkin/Factories/huesped/HuespedFactory.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Checkin.Model.Huesped;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reserva.Domain.Factories.cliente
+{
+    public class HuespedFactory : IHuespedFactory
+    {
+        public Huesped CrearHuesped()
+        {
+            return new Huesped();
+        }
+
+        public Huesped CrearHuesped(Guid? huespedId, string dni)
+        {
+            if (huespedId == null || huespedId == Guid.Empty)
+            {
+                return new Huesped();
+            }
+            return new Huesped(huespedId.Value,  dni);
+        }
+    }
+}
---
+++ backend/CheckIn/Domain.Checkin/Model/Huesped/Huesped.cs
@@ -0,0 +1,29 @@
+﻿using ShareKernel.Core;
+using ShareKernel.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Checkin.Model.Huesped
+{
+    public class Huesped : AggregateRoot<Guid>
+    {
+        //public Guid ClienteId { get; private set; }
+        public DniValue? DNI { get; private set; }
+        public Guid? HuespedId { get; set; }
+
+        public Huesped(Guid huespedId,string dni)
+        {
+            Id = Guid.NewGuid();
+            DNI = dni;
+            HuespedId = huespedId;
+        }
+
+        
+        // Only for Entity Framework
+        public Huesped() { }
+
+    }
+}
