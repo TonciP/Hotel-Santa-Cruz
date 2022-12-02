059ab19609f5eb125222087d1d252169435b92c9
TonciP <63807995+TonciP@users.noreply.github.com>
2022-12-02 18:10:59 -0400

    froent hotel init

---
+++ hotel/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CheckOut from './pages/checkout/Checkout';
+
+function App() {
+  return (
+    <div className="App">
+      <CheckOut />
+    </div>
+  );
+}
+
+export default App;
